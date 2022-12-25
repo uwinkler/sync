eff20371d453755b38cbd33708376e2fe25d5e0d
uli.winkler <uli.winkler@gmail.com>
2022-12-25 12:49:29 +0100

    stash

---
+++ server/build/src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('Hi from server/src/index.ts');
---
+++ server/src/index.ts
@@ -0,0 +1,1 @@
+console.log('Hi from server/src/index.ts')
